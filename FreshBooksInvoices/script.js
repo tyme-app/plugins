9dc105008004c6d201789647d723510bdae03972
Tyme Dev <dev@tyme-app.com>
2025-11-10 16:47:13 +0100

    implemented first oauth2 draft

--- FreshBooksInvoices/script.js
+++ FreshBooksInvoices/script.js
@@ -1,66 +1,89 @@
-class FreshBooksAPIClient {
-    constructor() {
-        // this.pluginAuthURL = "https://staging.tyme-app.com/plugins/oauth/";
-        this.pluginAuthURL = "http://localhost:8888/plugins/auth/";
-        this.freshBooksBaseURL = 'https://api.freshbooks.com/';
-        this.authCodeKey = 'code';
+class OAuthAPIClient {
+    constructor(pluginID) {
+        this.pluginID = pluginID;
+        this.pluginAuthURL = "http://localhost:8888/plugins/";
+        this.authCodeKey = 'auth_code';
+        this.accessTokenKey = 'access_token';
+        this.refreshTokenKey = 'refresh_token';
     }
 
     hasAuthCode() {
         return tyme.getSecureValue(this.authCodeKey) != null;
     }
 
-    startAuthFlow() {
-        tyme.openURL(this.pluginAuthURL + 'auth_start/freshbooks');
+    hasTokens() {
+        return tyme.getSecureValue(this.accessTokenKey) != null;
     }
 
-    fetchTokens() {
-        const url = this.pluginAuthURL + 'auth_code/freshbooks';
+    startAuthFlow() {
+        tyme.openURL(this.pluginAuthURL + 'auth_start/' + this.pluginID);
+    }
+
+    fetchTokensFromCode() {
+        const url = this.pluginAuthURL + 'auth_code/' + this.pluginID;
         const code = tyme.getSecureValue(this.authCodeKey);
-        const response = utils.request(url, 'POST', {'code': code}, params);
+        const response = utils.request(url, 'POST', {}, {'code': code});
         const statusCode = response['statusCode'];
         const result = response['result'];
+
+        tyme.showAlert("codeexchange", JSON.stringify(response));
 
         // tyme.setSecureValue(this.authCodeKey, null);
 
         if (statusCode === 200) {
             const json = JSON.parse(result);
+            tyme.setSecureValue(this.accessTokenKey, json['access_token']);
+            tyme.setSecureValue(this.refreshTokenKey, json['refresh_token']);
             return true;
         } else {
-            utils.log('FreshBooks Auth Error ' + JSON.stringify(response));
+            utils.log('Code Exchange Error ' + JSON.stringify(response));
+            tyme.setSecureValue(this.accessTokenKey, null);
+            tyme.setSecureValue(this.refreshTokenKey, null);
             return false;
         }
     }
 
     refreshTokens() {
-        const url = this.pluginAuthURL + 'refresh_token/freshbooks';
-        const code = tyme.getSecureValue(this.authCodeKey);
-        const response = utils.request(url, 'POST', {'code': code}, params);
+        const url = this.pluginAuthURL + 'refresh_token/' + this.pluginID;
+        const refreshToken = tyme.getSecureValue(this.refreshTokenKey);
+        const response = utils.request(url, 'POST', {}, {'refresh_token': refreshToken});
         const statusCode = response['statusCode'];
         const result = response['result'];
 
-        // tyme.setSecureValue(this.authCodeKey, null);
-
         if (statusCode === 200) {
             const json = JSON.parse(result);
+            tyme.setSecureValue(this.accessTokenKey, json['access_token']);
+            tyme.setSecureValue(this.refreshTokenKey, json['refresh_token']);
             return true;
         } else {
-            utils.log('FreshBooks Auth Error ' + JSON.stringify(response));
+            utils.log('Token Refresh Error ' + JSON.stringify(response));
+            tyme.setSecureValue(this.accessTokenKey, null);
+            tyme.setSecureValue(this.refreshTokenKey, null);
             return false;
         }
     }
 }
 
-const apiClient = new FreshBooksAPIClient();
 
 class FreshBooks {
-    constructor(apiClient) {
-        this.apiClient = apiClient;
+    constructor(oAuthAPIClient) {
+        this.oAuthAPIClient = oAuthAPIClient;
+    }
+
+    generatePreview() {
+
+        if(oAuthAPIClient.hasAuthCode()) {
+            oAuthAPIClient.fetchTokensFromCode();
+        }
+
+        return "hasAuthCode: " + oAuthAPIClient.hasAuthCode() +
+            ", hasTokens: " + oAuthAPIClient.hasTokens();
     }
 
     startAuthFlow() {
-
+        this.oAuthAPIClient.startAuthFlow();
     }
 }
 
-const freshBooks = new FreshBooks(apiClient);
+const oAuthAPIClient = new OAuthAPIClient("freshbooks");
+const freshBooks = new FreshBooks(oAuthAPIClient);
